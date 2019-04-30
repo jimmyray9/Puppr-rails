2c00134c63e7d57c0e22b9c06d51f91a8c60001b
James Gilbert <jimmyray9@gmail.com>
2019-04-30 14:24:40 +0800

    initial commit

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.11'
 gem 'rest-client'
 gem 'jwt'
+gem 'faker'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+    user = Walker.new({
+        name: Faker::FunnyName.name,
+        phone_number: Faker::PhoneNumber.phone_number,
+        is_walker: true,
+        walker_age: rand(19..35),
+        walker_description: Faker::Movie.quote,
+        zip_code: [610000, 610012, 610013, 610043, 610036].sample,
+        walker_img: Faker::Internet.url
+    })
+    user.save
+end
+
+50.times do
+    user = Owner.create({
+        name: Faker::FunnyName.name,
+        phone_number: Faker::PhoneNumber.phone_number,
+        zip_code: [610000, 610012, 610013, 610043, 610036].sample
+    })
+    user.save
+    dog = Dog.create({
+        name: ,
+        breed: ,
+        age: 1 + rand(17) ,
+        description: ,
+        size: ['XS','S', 'M', 'L','XL'].sample,
+        gender: ['M', 'F'].sample,
+        pic: ''
+    })
+end
