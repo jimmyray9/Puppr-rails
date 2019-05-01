517a8bca791797f90fb99a97d37261e04b97eb76
PEILI JIANG <1281277805@qq.com>
2019-05-01 14:36:20 +0800

    database

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,2 +1,59 @@
 class AppointmentsController < ApplicationController
+    before_action :set_appointment, only: [:show, :update, :destroy]
+    def index
+        @walker = walker.find(params[:walker_id])
+        @appointments = Appointment.all
+        @array = []
+        @appointments.each do |appointment|
+        @array << appointment if appointment.walker == @walker
+        end
+        render json: @array
+    end
+
+    def show
+        @walker = walker.find(params[:walker_id])
+        render json: @appointment if @appointment.walker == @walker
+    end
+
+    def update
+        @walker = walker.find(params[:walker_id])
+        @appointment.walker = @walker
+
+        if @appointment.update(appointment_params)
+        render json: @appointment
+        else
+        render_error
+        end
+    end
+
+    def create
+        @appointment = Appointment.new(appointment_params)
+        @walker = walker.find(params[:walker_id])
+        @appointment.walker = @walker
+
+        if @appointment.save
+        render json: @appointment, status: :created
+        else
+        render_error
+        end
+    end
+
+    def destroy
+        @appointment.destroy
+        head :no_content
+    end
+
+    private
+
+    def set_appointment
+        @appointment = Appointment.find(params[:id])
+    end
+
+    def appointment_params
+        params.require(:appointment).permit(:duration, :note, :start_time, :date, :location, :is_scheduled, :comment, :rating :address, :duration, :walker_id)
+    end
+
+    def render_error
+        render json: { errors: @appointment.errors.full_messages }, status: :unprocessable_entity
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/login', to: "login#login"
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :owners do
+        resources :dogs
+      end
+      resources :walkers do
+        resources :appointments
+      end
+    end
+  end
 end
