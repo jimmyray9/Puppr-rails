c3d106d41f369d74e94c5edfb5529cf2a0f72daa
PEILI JIANG <1281277805@qq.com>
2019-05-08 11:21:19 +0800

    .

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,7 @@ class Appointment < ApplicationRecord
   has_many :walks, dependent: :destroy
   has_many :dogs, through: :walks, dependent: :destroy
   has_many :trackings, dependent: :destroy
-  validates :location, presence: true
-  validates :note, presence: true
-  validates :duration, presence: true
+  # validates :location, presence: true
+  # validates :note, presence: true
+  # validates :duration, presence: true
 end
