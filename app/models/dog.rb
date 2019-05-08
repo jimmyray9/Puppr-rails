901af92af51ea129671b31dec33566adb9c50142
PEILI JIANG <1281277805@qq.com>
2019-05-08 10:33:30 +0800

    .

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,4 +2,8 @@ class Dog < ApplicationRecord
   belongs_to :owner
   has_many :walks, dependent: :destroy
   has_many :appointments, through: :walks, dependent: :destroy
+  validates :name, presence: true
+  validates :pic, presence: true
+  validates :age, presence: true
+  validates :description, presence: true
 end
